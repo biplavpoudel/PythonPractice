ae3774a05b76982ccce45d8921f48602c0acfde3
Biplav <biplavpoudel764@gmail.com>
2022-04-22 11:41:44 +0545

    Dictionary Comparision

---
+++ greater_dict.py
@@ -0,0 +1,25 @@
+# Program to compare values in dictionary and print the key corresponding to largest value
+"""dict = {
+    'a':1,
+    'b':2,
+    'c':5}
+
+for i in dict.values():
+    print(i)
+
+max = 0  
+for i,j in dict.items():      
+    if int(j) > max:
+        largest = i
+        
+print("THe key corresponding to largest value is :",largest)
+
+"""
+
+list = [15,45,47,541,75]
+max1 = 0
+for items in list:
+    if max1 < items:
+        max1 = items
+
+print("The largest item in list is: ", max1)
