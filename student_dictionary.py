1c5c680882d7f43dc4a9667625c53b61644bda72
Biplav <biplavpoudel764@gmail.com>
2022-04-22 12:28:00 +0545

    aStudent_Dictionary

---
+++ student_dictionary.py
@@ -0,0 +1,9 @@
+student = {
+    "Name" : {"firstname":"Sharad","lastname":"Shrestha"},
+    "RollNo": 20,
+    "Class" : "CSIT",
+    "Subject" : ["ToC","AI","Computer Network","Python","DBMS"]
+}
+
+print(student["Subject"][1])
+print(student["Name"]["lastname"])
